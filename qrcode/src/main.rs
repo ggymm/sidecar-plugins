d68888c294c235d0558407f5255c1168a5a53509
gongym <1967988842@qq.com>
2025-05-10 18:17:47 +0800

    update

--- qrcode/src/main.rs
+++ qrcode/src/main.rs
@@ -1,3 +1,40 @@
+use std::env;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("Usage: {} <qrcode image path>", args[0]);
+        process::exit(1);
+    }
+
+    let path = &args[1];
+
+    // 读取图片
+    let img = match image::open(path) {
+        Ok(img) => img,
+        Err(e) => {
+            eprintln!("can not open image: {} {}", path, e);
+            process::exit(1);
+        }
+    };
+
+    let img = img.to_luma8();
+    let mut img = rqrr::PreparedImage::prepare(img);
+
+    let grids = img.detect_grids();
+    if grids.is_empty() {
+        eprintln!("can not find qrcode in image: {}", path);
+        process::exit(1);
+    }
+
+    let (_, content) = match grids[0].decode() {
+        Ok(decoded) => decoded,
+        Err(e) => {
+            eprintln!("can not decode qrcode in image: {} {}", path, e);
+            process::exit(1);
+        }
+    };
+    println!("{}", content);
 }
